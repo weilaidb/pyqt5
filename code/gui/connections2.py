10ef264a5c9514e443e9af1331befea8aeb15169
weiwei <wxjlmr@126.com>
2018-04-05 23:02:59 +0800

    1.1 解决信号发射问题

--- code/gui/connections2.py
+++ code/gui/connections2.py
@@ -2,29 +2,26 @@ import sys
 import urllib.request
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-
+from PyQt5.QtCore import QObject, pyqtSignal
 
 class ZeroSpinBox(QSpinBox):
     zeros = 0
+    atzero = pyqtSignal(int)  # 定义信号,定义参数为int类型
 
     def __init__(self, parent = None):
         super(ZeroSpinBox, self).__init__(parent)
         self.valueChanged.connect(self.checkzero)
-        self._signal.atzero.connect(self.announce)
+        self. atzero.connect(self.announce)
 
     def checkzero(self):
         if self.value() == 0:
             self.zeros += 1
-            # self.
-            self._signal.atzero.emit("atzero")
-        print("%d" % self.zeros)
-            # self._signal.connect(self.zeros)
-            # self._signal.emit("atzero")
-            # self.atzero.connect(self.zeros)
-        # self.atzero.connect(self.announce)
+            self.atzero.emit(self.zeros)
+        # print("%d" % self.zeros)
 
-    def announce(self):
-        print("ZeroSpinBox has been at zero %d times" % self.zeros)
+
+    def announce(self,zeros):
+        print("ZeroSpinBox has been at zero %d times" % zeros)
 
 
 
