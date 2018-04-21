8c75c287ba0423c78c372ed3bbf4ca99682f72d4
weiwei <wxjlmr@126.com>
2018-04-21 18:59:21 +0800

    创建生成代码的界面

---
+++ code/gencode/main.py
@@ -0,0 +1,13 @@
+#pyqtmain pyqt main
+from mainui import *
+import math 
+
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    ftpw = MainWindow()
+    ftpw.show()
+    sys.exit(app.exec_())
+
