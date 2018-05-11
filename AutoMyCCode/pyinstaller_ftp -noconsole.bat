88834c3f810fdccfb112f9e3b12f104b18b50e18
weiwei <wxjlmr@126.com>
2018-05-11 23:06:20 +0800

    程序文件 添加ico

--- AutoMyCCode/pyinstaller_ftp -noconsole.bat
+++ AutoMyCCode/pyinstaller_ftp -noconsole.bat
@@ -1,6 +1,6 @@
 @echo on
 @echo pyinstaller db/main.py
-pyinstaller --noconsole   db/main.py
+pyinstaller --noconsole  -i akregator.ico db/main.py
 @echo pyinstaller db/main.py done!!!
 
 pause
--- AutoMyCCode/pyinstaller_ftp.bat
+++ AutoMyCCode/pyinstaller_ftp.bat
@@ -1,6 +1,6 @@
 @echo on
 @echo pyinstaller db/main.py
-pyinstaller --noconsole   db/main.py -F
+pyinstaller --noconsole   -i akregator.ico  db/main.py -F
 @echo pyinstaller db/main.py done!!!
 
 pause
